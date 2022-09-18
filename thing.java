6a70bf67d9a416e871e2afd6c15c4a7883611bfd
cihsgame <108897594+cihsgame@users.noreply.github.com>
2022-09-18 11:52:00 -0400

    Create thing.java

---
+++ thing.java
@@ -0,0 +1,12 @@
+public class squarenumber {
+  static void square(int tobesquared) {
+    System.out.println(tobesquared*tobesquared);
+  }
+  public static void main(String[] args) {
+    square(1);
+    square(2);
+    square(3);
+    square(4);
+    square(5);
+  }
+}
