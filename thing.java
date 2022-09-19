83d13ec4c53a1f5b9ef23ce78d22601184449cfb
cihsgame <108897594+cihsgame@users.noreply.github.com>
2022-09-19 15:07:28 -0400

    Update thing.java

--- thing.java
+++ thing.java
@@ -28,3 +28,18 @@ public class tictactoe
       System.out.println("          |		 |");
    }
 }
+
+public class box
+{
+   public static void main (String[] args)
+   {
+      System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+      System.out.println("\\             \\");
+      System.out.println("\\             \\");
+      System.out.println("\\             \\");
+      System.out.println("\\             \\");
+      System.out.println("\\             \\");
+      System.out.println("\\             \\");
+      System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+   }
+}
